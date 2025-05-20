ac59fc01a63443bd6fe5e6df98061a6148aa306a
KevinXu-HZ <northwindxu@gmail.com>
2025-05-19 21:58:57 -0700

    Add new feature that developer can access database at anytime from source code

--- Homework_1/main.py
+++ Homework_1/main.py
@@ -1,8 +1,31 @@
-from fastapi import FastAPI, HTTPException
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 import uvicorn
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./messages.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class MessageModel(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String, unique=True, index=True)
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
@@ -16,6 +39,8 @@ app.add_middleware(
 class Message(BaseModel):
     id: int
     text: str
+    class Config:
+        orm_mode = True
 
 class MessageCreate(BaseModel):
     text: str
@@ -30,18 +55,21 @@ async def root():
     return {"status": "ok", "message": "FastAPI server is running"}
 
 @app.get("/api/messages", response_model=List[Message])
-async def get_messages():
-    return messages_db
+async def get_messages(db: Session = Depends(get_db)):
+    messages = db.query(MessageModel).all()
+    return messages
+
 
 @app.post("/api/messages", response_model=Message, status_code=201)
-async def create_message(message: MessageCreate):
-    if any (m.text == message.text for m in messages_db):
+async def create_message(message: MessageCreate, db: Session = Depends(get_db)):
+    db_message = db.query(MessageModel).filter(MessageModel.text == message.text).first()
+    if db_message:
         raise HTTPException(status_code=400, detail=f"Message with text \"{message.text}\" already exists")
-    else:
-        new_id = max([m.id for m in messages_db], default=0) + 1
-        new_message = Message(id=new_id, text=message.text)
-        messages_db.append(new_message)
-        return new_message
+    new_message = MessageModel(text=message.text)
+    db.add(new_message)
+    db.commit()
+    db.refresh(new_message)
+    return new_message
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
